bf641b3463baca8c71d1a77207a2996283c75a40
12ME34 <15521@edu.szf.at>
2022-11-18 11:40:44 +0100

    Add project files.

---
+++ Snackautomat/Drinks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snackautomat
+{
+    class Drinks
+    {
+        public Drinks()
+        {
+        }
+
+        public Drinks(string name, double size, double price, int calories, int sugar)
+        {
+            Name = name;
+            Size= size;
+            Price = price;
+            Calories = calories;
+            Sugar = sugar;
+
+        }
+        public string Name { get; set; }
+        public double Size { get; set; }
+        public double Price { get; set; }
+        public int Calories { get; set; }
+        public int Sugar { get; set; }
+
+
+    }   
+}
+    
+
